442e516fb5cc801184eb4b1783245d547c096647
Bicky <Bicky@codewithbicky.com>
2022-06-27 20:23:29 +0530

    develop branch test commit

--- src/main/java/com/codewithpreet/blog/payloads/PostDto.java
+++ src/main/java/com/codewithpreet/blog/payloads/PostDto.java
@@ -2,9 +2,15 @@ package com.codewithpreet.blog.payloads;
 
 import com.codewithpreet.blog.entities.Category;
 import com.codewithpreet.blog.entities.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Date;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class PostDto {
 
     private String title;
