987dfa678ca9153f1b4bed746734645923e8c946
Bicky <Bicky@codewithbicky.com>
2022-07-04 23:32:33 +0530

    User Role Update V3

--- src/main/java/com/codewithpreet/blog/payloads/UserDto.java
+++ src/main/java/com/codewithpreet/blog/payloads/UserDto.java
@@ -1,5 +1,6 @@
 package com.codewithpreet.blog.payloads;
 
+import com.codewithpreet.blog.entities.Role;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,6 +8,8 @@ import lombok.Setter;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
 
 @NoArgsConstructor
 @Getter
@@ -25,5 +28,7 @@ public class UserDto {
     private String password;
     @NotEmpty
     private String about;
+    private Set<RoleDto> roles=new HashSet<>();
+
 
 }
